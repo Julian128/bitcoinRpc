b904ac352fb0086d8a71bc10b2e4fd2ddafe45de
Julian <julianmoik@gmail.com>
2025-01-18 16:47:27 +0000

    rename sandbox to example

---
+++ example.py
@@ -0,0 +1,15 @@
+from decimal import Decimal
+from src.bitcoin import Bitcoin
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read("/home/julian/bitcoin/bitcoinPy/bitcoinPy.conf")
+user = config['settings']['user']
+password = config['settings']['password']
+host = config['settings']['host']
+port = config['settings']['port']
+bitcoin = Bitcoin(user, password, host, port)
+
+
+latestBlock = bitcoin.getLatestBlock()
+print(latestBlock)
